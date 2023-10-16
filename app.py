6c66631b125410de28eb1a1382524ed6c66053e0
CJ Nwuju <chukwudi.nwuju@kibo.school>
2023-10-16 16:55:20 +0100

    second commit

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<p>This is the fizzbuzz website, add (/fizzbuzz) t</p>"
+
 @app.route("/fizzbuzz")
 def fizzbuzz ():
     nonfizzbuzz = []
+    fizz = []
+    buzz = []
+    fizzbuzz = []
 
     for i in range(1,101):
         if i % 3 == 0 and i % 5 == 0 :
             print(f"{i} fizzbuzz")
+            a = f("{i} fizzbuzz")
+            fizzbuzz.append(a)
         elif i % 3 == 0 :
             print(f"{i} fizz")
+            b = f("{i} fizz")
+            fizz.append(b)
         elif i % 5 == 0 :
             print(f"{i} buzz")
+            c = f("{i} buzz")
+            buzz.append(c)
         else:
             nonfizzbuzz.append(i)
 
-    return nonfizzbuzz
+    return (nonfizzbuzz, fizz, buzz, fizzbuzz,)
